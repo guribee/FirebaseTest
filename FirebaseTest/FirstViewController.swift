11cf3f04a4d35b3c5b395bf5df799946cfa05eab
Yuki Aizawa <nunu6ew@gmail.com>
2019-11-16 17:44:10 +0900

    Initial Commit

---
+++ FirebaseTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FirebaseTest
+//
+//  Created by 会澤勇気 on 2019/11/16.
+//  Copyright © 2019 会澤勇気. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebaseTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FirebaseTest
+//
+//  Created by 会澤勇気 on 2019/11/16.
+//  Copyright © 2019 会澤勇気. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
